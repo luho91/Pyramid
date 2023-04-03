ce0eab2f7bde738ad7cde5fddedb6b371723d80b
Lukas Holzheid <lukas.holzheid@vcat.de>
2023-04-03 10:23:53 +0200

    Add core classes

--- src/Dot.java
+++ src/Dot.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Dot {
+/**
+ * Drawable dot in R². Has double and int coordinates.
+ * The double value is for calculations.
+ * The int value is for drawing.
+ */
+public class Dot {
+
+    double x;
+    double y;
+    double z;
+    /*
+    int transX;
+    int transY;
+    int transZ;*/
+
+    public Dot (double myX, double myY, double myZ) {
+        this.x = myX;
+        this.y = myY;
+        this.z = myZ;
+
+        // this.transX =
+    }
 }
